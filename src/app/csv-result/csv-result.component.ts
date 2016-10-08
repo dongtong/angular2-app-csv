7f1e9491330c8ba3d70215a3178dda496d8a4809
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2016-10-08 10:43:59 +0200

    csv changes heared

--- src/app/csv-result/csv-result.component.ts
+++ src/app/csv-result/csv-result.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-csv-result',
   templateUrl: './csv-result.component.html',
   styleUrls: ['./csv-result.component.css']
 })
-export class CsvResultComponent implements OnInit {
+export class CsvResultComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    console.log("Things changed");
   }
-
+  @Input() csvValues: string[][]
   onHearingChanges(payload: string[][]): void {
     console.log("changes heared, length in Parent:" + payload.length);
+    this.csvValues = payload;
+    
   }
 }
